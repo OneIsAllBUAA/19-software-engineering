12f628a329edec1a0df4f5e2e95b720a23981361
OneIsAll <16061002@buaa.edu.cn>
2019-04-06 14:23:10 +0800

    update

---
+++ login/login-71e023a0591707e427ad79390f64b02258b9bb1b/login/models.py
@@ -0,0 +1,125 @@
+# login/models.py
+from django.db import models
+from django.utils import timezone
+import hashlib
+
+
+def gen_md5(s, salt='9527'):  # 加盐
+    s += salt
+    md5 = hashlib.md5()
+    md5.update(s.encode(encoding='utf-8'))  # update方法只接收bytes类型
+    return md5.hexdigest()
+
+
+def file_directory_path(instance, filename):
+    # 文件上传到MEDIA_ROOT/task_<id>/<filename>目录中
+    return 'task_{0}/{1}.{2}'.format(instance.task.id, instance.id, filename.split('.')[-1])
+
+
+def screenshot_directory_path(instance, filename):
+    # 文件上传到MEDIA_ROOT/task_<id>/<filename>目录中
+    return 'task_{0}/{1}/{2}'.format(instance.task.id, instance.id, filename)
+
+
+def get_untagged_sub_task(task, user):
+    untagged_sub_task_set = task.subtask_set.exclude(users__id=user.id)
+    print('untagged_sub_task_set', untagged_sub_task_set)
+    return untagged_sub_task_set.first()  # if not exist, return None.
+
+
+def get_rejected_sub_task(task, user):
+    rejected_sub_task_set = task.subtask_set.filter(users__id=user.id, label__is_rejected=True).distinct()
+    print('rejected_sub_task_set', rejected_sub_task_set)
+    return rejected_sub_task_set.first()  # if not exist, return None.
+
+
+def get_unreviewed_sub_task(task, user):
+    unreviewed_sub_task_set = task.subtask_set.filter(users__id=user.id, label__is_unreviewed=True).distinct()
+    print('unreviewed_sub_task_set', unreviewed_sub_task_set)
+    return unreviewed_sub_task_set.first()  # if not exist, return None.
+
+
+class User(models.Model):
+    name = models.CharField(max_length=128, unique=True)
+    password = models.CharField(max_length=128)
+    email = models.EmailField(unique=True)
+    is_admin = models.BooleanField(default=False)
+    c_time = models.DateTimeField(auto_now_add=True)  # 保存用户创建时间
+    login_time = models.DateTimeField(default=timezone.now)  # 保存此次登录时间
+    last_login_time = models.DateTimeField(default=timezone.now)  # 保存上次登录时间
+
+    # favorite_tasks = models.ManyToManyField('Task')
+    total_credits = models.IntegerField(default=1000)
+    num_label_accepted = models.IntegerField(default=1)
+
+    def __str__(self):
+        return self.name
+
+    class Meta:
+        ordering = ["c_time"]
+
+
+class Task(models.Model):
+    """任务表"""
+    type = models.IntegerField(default=1)
+    template = models.IntegerField(default=1)
+    content = models.TextField(max_length=1024)
+    name = models.CharField(max_length=128)
+    admin = models.ForeignKey('User', on_delete=models.CASCADE, null=True, related_name='released_tasks')
+    details = models.TextField(max_length=1024)
+    c_time = models.DateTimeField(auto_now_add=True)
+    max_tagged_num = models.IntegerField(default=1)
+    is_closed = models.BooleanField(default=False)
+    credit = models.IntegerField(default=1)
+    users = models.ManyToManyField('User', related_name='favorite_tasks', through='TaskUser')
+    user_level = models.IntegerField(default=1)
+
+    def __str__(self):
+        return self.name
+
+    class Meta:
+        ordering = ["c_time"]
+
+
+class TaskUser(models.Model):
+    task = models.ForeignKey('Task', on_delete=models.CASCADE, null=True)
+    user = models.ForeignKey('User', on_delete=models.SET_NULL, null=True)
+    # is_rejected = models.BooleanField(default=False)
+    # is_unreviewed = models.NullBooleanField(default=None)
+    c_time = models.DateTimeField(auto_now_add=True)
+
+    class Meta:
+        ordering = ["c_time"]
+
+
+class SubTask(models.Model):
+    """子任务表"""
+
+    file = models.FileField(max_length=256, upload_to=file_directory_path)
+    task = models.ForeignKey('Task', null=True, on_delete=models.CASCADE)
+    result = models.TextField(max_length=1024)  # 保存最终标记结果
+    # num_tagged = models.IntegerField(default=0)
+    users = models.ManyToManyField('User', related_name='sub_tasks_tagged', through='Label')
+
+
+class Label(models.Model):
+    """标签表"""
+
+    sub_task = models.ForeignKey('SubTask', on_delete=models.CASCADE, null=True)
+    user = models.ForeignKey('User', on_delete=models.SET_NULL, null=True)
+    result = models.TextField(max_length=1024)  # 保存标记结果
+    m_time = models.DateTimeField(auto_now=True)  # 保存最后标记时间
+    # is_tagged = models.BooleanField(default=False)
+    is_rejected = models.BooleanField(default=False)
+    is_unreviewed = models.BooleanField(default=True)
+    task_user = models.ForeignKey('TaskUser', on_delete=models.CASCADE, null=True)
+
+    class Meta:
+        ordering = ["m_time"]
+
+
+class Screenshot(models.Model):
+    sub_task = models.ForeignKey('SubTask', on_delete=models.CASCADE, null=True)
+    label = models.ForeignKey('Label', on_delete=models.CASCADE, null=True)
+    result = models.TextField(max_length=1024)  # 保存标记结果
+    image = models.FileField(max_length=256)
---
+++ login/login-71e023a0591707e427ad79390f64b02258b9bb1b/mysite/urls.py
@@ -0,0 +1,57 @@
+"""mysite URL Configuration
+
+The `urlpatterns` list routes URLs to views. For more information please see:
+    https://docs.djangoproject.com/en/2.0/topics/http/urls/
+Examples:
+Function views
+    1. Add an import:  from my_app import views
+    2. Add a URL to urlpatterns:  path('', views.home, name='home')
+Class-based views
+    1. Add an import:  from other_app.views import Home
+    2. Add a URL to urlpatterns:  path('', Home.as_view(), name='home')
+Including another URLconf
+    1. Import the include() function: from django.urls import include, path
+    2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
+"""
+from django.contrib import admin
+from django.urls import path
+from login import views
+from django.conf import settings
+from django.conf.urls.static import static
+
+urlpatterns = [
+    path('admin/', admin.site.urls),
+
+    path('', views.index),
+    path('index/', views.index),
+    path('login/', views.login),
+    path('regist/', views.register),
+    path('logout/', views.logout),
+
+    path('choose/', views.choose),
+    path('release_task/', views.release_task),
+    path('release_task_1/', views.release_task_1),
+    path('release_task_2/', views.release_task_2),
+    path('release_task_3/', views.release_task_3),
+    path('release_task_4/', views.release_task_4),
+    path('video2pictures_slide/', views.video2pictures_slide),
+    path('confirm_to_upload_pictures/', views.confirm_to_upload_pictures),
+
+    path('all_task/', views.all_task),
+
+    path('enter_task/', views.enter_task),
+    path('picture_task/', views.picture_task),
+    path('video_task/', views.video_task),
+    path('player_task/', views.player_task),
+
+    path('one_task/', views.one_task),
+    path('check_task/', views.check_task),
+    path('picture_detail/', views.picture_detail),
+
+    path('recharge/', views.recharge),
+    path('download_data_set/', views.download_data_set),
+
+    # path('task/get_all_tasks/', views.get_all_tasks, name='get_all_tasks'),
+    # path('task/get_user_tasks/', views.get_user_tasks, name='get_user_tasks'),
+
+] + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
