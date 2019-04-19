968f79ed9d527b1a8bc118118820edcdb5666a4f
ellewater <49151287+ellewater@users.noreply.github.com>
2019-04-19 23:37:40 +0800

    API

--- app/src/main/java/com/oneisall/Constants/Templates.java
+++
@@ -1,8 +0,0 @@
-package com.oneisall.Constants;
-
-public class Templates{
-    public static final int VIDEO = 1;
-    public static final int IMG = 2;
-    public static final int AUDIO = 3;
-    public static final int NON = 0;
-}
