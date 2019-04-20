a51cc2a193b2c264115d1da7c293c0b84d5f40e4
asevergreen <2608135589@qq.com>
2019-04-20 18:00:13 +0800

    api

--- Login/login/admin.py
+++
@@ -1,9 +0,0 @@
-# login/admin.py
-
-from django.contrib import admin
-from . import models
-
-admin.site.register(models.User)
-admin.site.register(models.Task)
-admin.site.register(models.SubTask)
-admin.site.register(models.Label)
