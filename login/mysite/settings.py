11cc485b95c0a435e73eff782179f4e3ed65c446
rasalghul <liao13668009346@163.com>
2019-05-11 20:02:55 +0800

    3

--- login/mysite/settings.py
+++ login/mysite/settings.py
@@ -60,7 +60,7 @@ CHANNEL_LAYERS = {
     'default': {
         'BACKEND': 'channels_redis.core.RedisChannelLayer',
         'CONFIG': {
-            "hosts": [("redis-server-name", 6379)],
+            "hosts": [("localhost", 6379)],
         },
     },
 }
