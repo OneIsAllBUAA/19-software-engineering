b61fab039abd22d5dafb01400fab0bd6cd8a84c9
OneIsAll <16061002@buaa.edu.cn>
2019-06-02 23:52:32 +0800

    Revert "Merge branch 'lsh' of https://github.com/OneIsAll980516/19-software-engineering into lsh"
    This reverts commit d3b0d88a872da03142fa43354ce579e93574e86e, reversing
    changes made to e138f7b6ee5f1880e01e0b0f29a8a26ed818e259.

--- login/login/views.py
+++ login/login/views.py
@@ -1861,8 +1861,7 @@ def api_submit_task(request):
         current_task.num_worker += 1
         current_task.save()
         task_user = models.TaskUser.objects.create(task=current_task, user=current_user)
-    task_user.num_label_unreviewed = current_task.subtask_set.all().count()
-    task_user.num_label_rejected = 0
+        task_user.num_label_unreviewed = current_task.subtask_set.all().count()
     ind = 0
     try:
         for ans in req["answer"]:
